bc53524ac67eb5281ac31bab1fdb52e47c8eece5
virtueer <22412769+virtueer@users.noreply.github.com>
2026-07-12 20:52:58 +0300

    feat: add AttachScreen component to display container terminal session

--- frontend/src/components/AttachScreen.tsx
+++ frontend/src/components/AttachScreen.tsx
@@ -29,7 +29,7 @@ export function AttachScreen({ containerId, containerName, shell }: AttachScreen
       </div>
 
       {/* Terminal Container */}
-      <div className="flex-1 min-h-0 overflow-hidden p-2 pb-8">
+      <div className="flex-1 min-h-0 overflow-hidden p-2 pb-2">
         <AttachTerminal containerId={containerId} shell={shell} />
       </div>
     </div>
